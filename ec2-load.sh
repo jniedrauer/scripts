bb9211d7bb5ae974f6dec67ac53b660358155d7b
Josiah Niedrauer <jniedrauer@dealersocket.com>
2018-02-22 13:45:35 -0600

    Better argument handling

--- ec2-load.sh
+++ ec2-load.sh
@@ -3,12 +3,53 @@
 set -eu
 
 main() {
-    local filters=("${@}")
-    [ -n "${filters[0]:-}" ] || print_help "$?"
-    [ "${filters[0]:-}" = '--help' ] && print_help "$?"
+    read_args "$@"
+    [ -n "${filters:-}" ] || print_help "$?"
 
     local instances=($(filters_instances "${filters[@]}"))
     get_output_for_humans "${instances[@]}" | sort -k2,2 -n
+}
+
+read_args() {
+    while [ "${#}" -gt 0 ]; do
+        case "${1}" in
+            -f|--filters|--filters=*)
+                if [ "${1#*=}" = "$1" ]; then
+                    shift
+                    while [ "${#}" -gt 0 ]; do
+                        case "${1}" in
+                            -*)
+                                break
+                                ;;
+                            *)
+                                filters+=("$1")
+                                shift
+                                ;;
+                        esac
+                    done
+                else
+                    filters+=("${1#*=}")
+                    shift
+                fi
+                ;;
+            --profile|--profile=*)
+                if [ "${1#*=}" = "$1" ]; then
+                    param="$2"
+                    shift
+                else
+                    param="${1#*=}"
+                fi
+                [ -z "${param:-}" ] && print_help 1
+                export AWS_PROFILE="$param"
+                shift
+                ;;
+            -h|--help)
+                print_help 0
+                ;;
+            *)
+                print_help 1
+        esac
+    done
 }
 
 get_output_for_humans() {
@@ -25,7 +66,7 @@ get_output_for_humans() {
 filters_instances() {
     local filters=("$@")
     aws ec2 describe-instances \
-        --filters "${filters[@]}" \
+        --filters "${filters[@]}" 'Name=instance-state-code,Values=16' \
         --query='Reservations[*].Instances[*].[InstanceId]' --output=text
 }
 
@@ -46,10 +87,12 @@ get_instance_average() {
 print_help() {
     local exit_status="$1"
     cat <<EOF
-usage: $0 FILTERS
+usage: $0 [-f,--filters FILTERS] [--profile AWS_PROFILE]
 
 Where FILTERS is a list of AWS EC2 filters.
-Example: $0 'Name=tag:Group,Values=groupname'
+Example: $0 --filters 'Name=tag:Group,Values=groupname' 'Name=tag:Name,Values=name'
+
+And AWS_PROFILE is a boto profile.
 EOF
     exit "$exit_status"
 }
